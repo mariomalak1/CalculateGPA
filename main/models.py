abe4456ad724b7bc56ba1a7caecc0e98ab37836b
mario malak <93185168+mariomalak1@users.noreply.github.com>
2024-01-30 14:08:13 +0200

    make migration

--- main/models.py
+++ main/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User as djangoUser
 # Create your models here.
 
 class Subject(models.Model):
     gradeChosies = (("A+", 4), ("A", 3.7), ("B+", 3.3), ("B", 3), ("C+", 2.7), ("C", 2.4), ("D+", 2), ("D", 2),)
     subName = models.CharField(max_length=120, null=False)
-    grade = models.PositiveSmallIntegerField(max_length=5, null=False, choices=gradeChosies)
-    student = models.ForeignKey("User", on_delete=models.CASCADE)
+    grade = models.PositiveSmallIntegerField(null=False, choices=gradeChosies)
+    student = models.ForeignKey(djangoUser, on_delete=models.CASCADE)
