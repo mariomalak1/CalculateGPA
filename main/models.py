b93f7dc1b0f6b1cc6ee0f89311b637d737dbb97b
mario malak <93185168+mariomalak1@users.noreply.github.com>
2024-03-07 20:27:31 +0200

    make subject name is unique

--- main/models.py
+++ main/models.py
@@ -8,7 +8,7 @@ class Subject(models.Model):
     gradeChosies = ((4, "A+"), (3.7, "A"), (3.3, "B+"), (3, "B"), (2.7, "C+"), (2.4, "C"), (2, "D+"), (2, "D"),)
     TermNumber = ((1, "First Term"), (2, "Second Term"),)
     YearNumber = ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7))
-    subject_name = models.CharField(max_length=120, null=False, blank=False)
+    subject_name = models.CharField(max_length=120, null=False, blank=False, unique=True)
     grade = models.FloatField(null=False, blank=False, choices=gradeChosies)
     term = models.PositiveSmallIntegerField(null=False, blank=False, choices=TermNumber)
     year = models.PositiveSmallIntegerField(null=False, blank=False, choices=YearNumber)
